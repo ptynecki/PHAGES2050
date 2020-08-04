aee73146ce5eb81dd4f4d935905ca831ad813095
Piotr Tynecki <contact@pypassion.com>
2020-08-04 21:04:35 +0200

    Added pypi initial setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'phages2050',
+    version = '0.0.1',
+    description = 'PHAGES2050 is a novel Python 3.6+ programming language framework to boost bacteriophage research & therapy and infrastructure in order to achieve the full potential to fight against antimicrobial resistant bacteria within Natural Language Processing (NLP) and Deep Learning.',
+    author = 'Piotr Tynecki',
+    author_email = 'p.tynecki@doktoranci.pb.edu.pl',
+    url = 'https://github.com/ptynecki/PHAGES2050',
+    download_url = 'https://github.com/ptynecki/PHAGES2050/archive/v0.0.1.tar.gz',
+    keywords = ['bacteriophages', 'phages', 'genome embedding', 'protein embedding', 'amr'],
+    classifiers = [],
+)
+
