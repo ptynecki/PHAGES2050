9c8094b8a0bc06aec0fdccbd6d410fd2622bba1d
Piotr Tynecki <contact@pypassion.com>
2020-08-11 00:40:53 +0200

    Included Back formatting results

--- crawlers/millardlab/crawler.py
+++ crawlers/millardlab/crawler.py
@@ -38,11 +38,14 @@ class MillardLabPhagesCrawler:
         self.url = url
         self.df = None
         today = date.today()
-        self.csv_name = f'millardlab_{today}.csv'
-        
+        self.csv_name = f"millardlab_{today}.csv"
+
         self.columns = [
-            'Accession', 'Description', 'Classification',
-            'Genome Length(bp)', 'molGC'
+            "Accession",
+            "Description",
+            "Classification",
+            "Genome Length(bp)",
+            "molGC",
         ]
 
     @staticmethod
@@ -51,27 +54,27 @@ class MillardLabPhagesCrawler:
         Return string without blank chars
         and in uppercase format
         """
-        
+
         return element.text.strip().upper()
-    
+
     def _get_html(self) -> str:
         """
         Request to MillardLab webiste URL
         and return HTML as string if
         status code is 200
         """
-        
+
         try:
             response = requests.get(self.url)
 
             if response.status_code == 200:
                 return response.text
             else:
-                print(f'[DEBUG] Status code is not valid: {response.status_code}')
-                
-                return ''
+                print(f"[DEBUG] Status code is not valid: {response.status_code}")
+
+                return ""
         except requests.exceptions.RequestException as e:
-            print(f'[DEBUG] Request exception: {e}')
+            print(f"[DEBUG] Request exception: {e}")
 
     def _process_element(self, document, xpath: str) -> map:
         """
@@ -84,72 +87,64 @@ class MillardLabPhagesCrawler:
         """
         Extract text from each table cell
         """
-        
+
         try:
             document = html.document_fromstring(self._get_html())
 
             # Accession (as hyperlink)
             acc_numbers = self._process_element(
-                document,
-                "//td[contains(@class, 'column-1')]/a"
+                document, "//td[contains(@class, 'column-1')]/a"
             )
 
             # Description
             descriptions = self._process_element(
-                document,
-                "//td[contains(@class, 'column-2')]"
+                document, "//td[contains(@class, 'column-2')]"
             )
 
             # Classification
             taxonomies = self._process_element(
-                document,
-                "//td[contains(@class, 'column-3')]"
+                document, "//td[contains(@class, 'column-3')]"
             )
 
             # Genome Length(bp)
             bps = self._process_element(
-                document,
-                "//td[contains(@class, 'column-4')]"
+                document, "//td[contains(@class, 'column-4')]"
             )
 
             # molGC
             gcs = self._process_element(
-                document,
-                "//td[contains(@class, 'column-5')]"
+                document, "//td[contains(@class, 'column-5')]"
             )
 
             samples = list(zip(acc_numbers, descriptions, taxonomies, bps, gcs))
         except ParserError:
             return []
-        
+
         return samples
-        
-    def to_df(self):
+
+    def to_df(self) -> pd.DataFrame:
         """
         Return data as DataFrame sorted
         by first column (Accession)
         """
-        
+
         samples = self._process_html()
-        
-        self.df = pd.DataFrame(
-            data=samples,
-            columns=self.columns
-        ).sort_values(
-            self.columns[0]
-        ).reset_index().drop(
-            'index',
-            axis=1
+
+        self.df = (
+            pd.DataFrame(data=samples, columns=self.columns)
+            .sort_values(self.columns[0])
+            .reset_index()
+            .drop("index", axis=1)
         )
 
         return self.df
 
-    def to_csv(self):
+    def to_csv(self) -> None:
         """
         Return DataFrame as CSV file
         (filename format: millardlab_<YYYY:MM:DD>.csv)
         """
-        
+
         self.to_df()
-        
+
         self.df.to_csv(self.csv_name, index=False)
